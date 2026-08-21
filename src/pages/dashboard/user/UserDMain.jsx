1fd0ef9e64efccc865a055483faf25df17cdb6eb
Moznur Rahman <moznu.aci22@gmail.com>
2026-08-21 09:38:37 +0600

    fix: correct statsApi import path depth in UserDMain

--- src/pages/dashboard/user/UserDMain.jsx
+++ src/pages/dashboard/user/UserDMain.jsx
@@ -10,8 +10,8 @@ import {
   Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
-import { useGetUserStatsQuery } from "../../../redux/features/starts/startsApi";
-import Loading from "../../../components/Loding";
+import { useGetUserStatsQuery } from "../../../redux/features/stats/startsApi";
+import Loading from "../../../../components/Loding";
 
 ChartJS.register(
   CategoryScale,
